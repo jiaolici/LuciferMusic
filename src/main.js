aca23aa5e36df75d4363712063d857877268cc43
jiaolici <1321869892@qq.com>
2020-05-25 21:28:13 +0800

    添加插件

--- src/main.js
+++ src/main.js
@@ -1,8 +1,19 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+//导入element组件库
+import ElementUI from "element-ui";
+
+//导入组件相关样式
+import "element-ui/lib/theme-chalk/index.css";
+
+import router from './router'
+//配置Vue插件
+Vue.use(ElementUI);
+
 Vue.config.productionTip = false
 
 new Vue({
-  render: h => h(App),
+  router,
+  render: h => h(App)
 }).$mount('#app')
