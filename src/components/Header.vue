33116c717930d9f8571f79ef217453ba7b01729f
jiaolici <1321869892@qq.com>
2020-05-26 23:28:33 +0800

    header

---
+++ src/components/Header.vue
@@ -0,0 +1,37 @@
+<template>
+    <div>
+        <el-menu
+            default-active="1"
+            class="el-menu-demo"
+            mode="horizontal"
+            @select="handleSelect"
+            background-color="#303133"
+            text-color="#fff"
+            active-text-color="#ffd04b">
+            <el-menu-item index="1">首页</el-menu-item>
+            <el-menu-item index="2">我的音乐</el-menu-item>
+        </el-menu>
+    </div>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+
+      };
+    },
+    methods: {
+      handleSelect(key, keyPath) {
+        console.log(key, keyPath);
+      }
+    }
+  }
+</script>
+
+<style scoped>
+div{
+    width: 1000px;
+    margin: 0 auto;
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,31 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <el-container>
+    <el-header>
+      <Header>
+      </Header>
+    </el-header>
+    <el-main>
+      main
+    </el-main>
+  </el-container>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import Header from '@/components/Header.vue'
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld
+    Header
   }
 }
 </script>
+
+<style scoped>
+  .el-header{
+    background-color: #303133;
+    color: #fff;
+    line-height: 60px;
+  }
+</style>
