af1e5a7b1cd51280917fdd5b43ac5fc14b4f8c37
jiaolici <1321869892@qq.com>
2020-05-27 23:19:42 +0800

    header2

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,24 +1,35 @@
 <template>
-    <div>
-        <el-menu
-            default-active="1"
-            class="el-menu-demo"
-            mode="horizontal"
-            @select="handleSelect"
-            background-color="#303133"
-            text-color="#fff"
-            active-text-color="#ffd04b">
-            <el-menu-item index="1">首页</el-menu-item>
-            <el-menu-item index="2">我的音乐</el-menu-item>
-        </el-menu>
-    </div>
+    <el-row>
+        <el-col :span="4"><h1>Lucifer Music</h1></el-col>
+        <el-col :span="8" :offset="1">
+            <el-menu
+                default-active="1"
+                class="el-menu-demo"
+                mode="horizontal"
+                @select="handleSelect"
+                background-color="#303133"
+                text-color="#fff"
+                active-text-color="#ffd04b">
+                <el-menu-item index="1">首页</el-menu-item>
+                <el-menu-item index="2">我的音乐</el-menu-item>
+            </el-menu>
+        </el-col>
+        <el-col :span="4">
+            <el-input placeholder="请输入内容" size="mini" v-model="inputText">
+                <i slot="prefix" class="el-input__icon el-icon-search"></i>
+            </el-input>
+        </el-col>
+        <el-col :span="4">
+            <el-avatar size="medium"> user </el-avatar>
+        </el-col>
+    </el-row>
 </template>
 
 <script>
   export default {
     data() {
       return {
-
+          inputText:""
       };
     },
     methods: {
@@ -30,8 +41,17 @@
 </script>
 
 <style scoped>
-div{
+.el-row{
     width: 1000px;
     margin: 0 auto;
+}
+.el-col{
+    height: 60px;
+}
+h1{
+    margin: 0;
+}
+.el-avatar{
+    margin-top: 12px;
 }
 </style>
