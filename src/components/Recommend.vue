d8a10b3df1bf3fc1cf88ba475fe929d033388427
jiaolici <1321869892@qq.com>
2020-05-29 17:40:17 +0800

    新专辑模块

--- src/components/Recommend.vue
+++ src/components/Recommend.vue
@@ -1,17 +1,43 @@
 <template>
   <div class="recommend">
         <h1>歌单推荐</h1>
+        <el-divider></el-divider>
         <el-row>
             <el-button type="text">民谣</el-button>
             <el-button type="text">新金属</el-button>
             <el-button type="text">朋克</el-button>
         </el-row>
         <el-row>
-        <div class="cover">
-            <el-image style="width: 100px; height: 100px">
-            </el-image>
-            <p><el-link :underline="false">无下划线</el-link></p>
-        </div>
+            <el-col :span="4">
+                <el-image style="width: 100px; height: 100px">
+                </el-image>
+                <p style="width: 100px;margin:0 auto"><el-link :underline="false">无下划线</el-link></p>
+            </el-col>
+            <el-col :span="4">
+                <el-image style="width: 100px; height: 100px">
+                </el-image>
+                <p style="width: 100px;margin:0 auto"><el-link :underline="false">无下划线</el-link></p>
+            </el-col>
+            <el-col :span="4">
+                <el-image style="width: 100px; height: 100px">
+                </el-image>
+                <p style="width: 100px;margin:0 auto"><el-link :underline="false">无下划线</el-link></p>
+            </el-col>
+            <el-col :span="4">
+                <el-image style="width: 100px; height: 100px">
+                </el-image>
+                <p style="width: 100px;margin:0 auto"><el-link :underline="false">无下划线</el-link></p>
+            </el-col>
+            <el-col :span="4">
+                <el-image style="width: 100px; height: 100px">
+                </el-image>
+                <p style="width: 100px;margin:0 auto"><el-link :underline="false">无下划线</el-link></p>
+            </el-col>
+            <el-col :span="4">
+                <el-image style="width: 100px; height: 100px">
+                </el-image>
+                <p style="width: 100px;margin:0 auto"><el-link :underline="false">无下划线</el-link></p>
+            </el-col>
         </el-row>
   </div>
 </template>
@@ -23,11 +49,5 @@ export default {
 </script>
 
 <style scoped>
-.cover{
-    width: 100px;
-}
-.recommend{
-    width: 1000px;
-    margin: 0 auto;
-}
+
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -7,6 +7,8 @@
     <el-main>
       <Recommend>
       </Recommend>
+      <Album>
+      </Album>
     </el-main>
   </el-container>
 </template>
@@ -15,12 +17,14 @@
 // @ is an alias to /src
 import Header from '@/components/Header.vue'
 import Recommend from '@/components/Recommend.vue'
+import Album from '@/components/Album.vue'
 
 export default {
   name: 'Home',
   components: {
     Header,
-    Recommend
+    Recommend,
+    Album
   }
 }
 </script>
@@ -30,5 +34,9 @@ export default {
     background-color: #303133;
     color: #fff;
     line-height: 60px;
+  }
+  .el-main{
+    width: 1000px;
+    margin: 0 auto;
   }
 </style>
