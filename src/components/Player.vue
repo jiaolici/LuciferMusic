98e55c435b0f4cf394679807c0f6d06d3fe84ca4
jiaolici <1321869892@qq.com>
2020-06-01 23:17:48 +0800

    播放器模块

--- src/components/Player.vue
+++ src/components/Player.vue
@@ -11,13 +11,33 @@
             <el-col :span="2">
                 <el-avatar shape="square" size="medium">封面</el-avatar>
             </el-col>
+            <el-col :span="12">
+                <el-slider v-model="music.currentTime" :max="music.maxTime" :format-tooltip="formatTime"></el-slider>
+            </el-col>
         </el-row>
     </div>
 </template>
 
 <script>
 export default {
-
+    data(){
+        return {
+            music:{
+                isPlay:false,
+                currentTime:0,
+                maxTime:180,
+                volume:100
+            }
+        }
+    },
+    methods:{
+        formatTime(time){
+            let it = parseInt(time)
+            let m = parseInt(it/60)
+            let s = parseInt(it%60)
+            return (m<10?"0":"")+parseInt(it/60)+":"+(s<10?"0":"")+parseInt(it%60)
+      }
+    }
 }
 </script>
 
@@ -41,5 +61,8 @@ export default {
 }
 .el-avatar{
     margin-top: 12px;
+}
+.el-slider{
+    margin-top: 11px;
 }
 </style>
