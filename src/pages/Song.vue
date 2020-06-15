b9866bef553ed0b8df747ad314fbc14558d4fdef
jiaolici <1321869892@qq.com>
2020-06-15 17:39:01 +0800

    歌曲详情页

--- src/pages/Song.vue
+++ src/pages/Song.vue
@@ -1,8 +1,12 @@
 <template>
-    <div style="width:600px">
+    <div>
         <el-image style="width:150px;height:150px;float:left"></el-image>
-        <div>
-            
+        <div style="padding-left:200px;text-align:left">
+            <h3>Insight</h3>
+            <div style="font-size:14px;">
+                歌手：
+                <el-link :underline="false" style="vertical-align: base-line;">Joy Division</el-link>
+            </div>
         </div>
     </div>
 </template>
@@ -13,6 +17,5 @@ export default {
 }
 </script>
 
-<style>
-
+<style scoped>
 </style>
