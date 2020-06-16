1318c30a8efe7d00bcfce1bf11ff16132685fe3d
jiaolici <1321869892@qq.com>
2020-06-17 00:04:01 +0800

    评论模块细化

--- src/components/Comment.vue
+++ src/components/Comment.vue
@@ -1,12 +1,57 @@
 <template>
     <div>
+        <div style="text-align:left">
+            <h4>
+                评论
+                <span style="font-size:14px;margin-left:15px;color:#999">
+                    共116691条评论
+                </span>
+            </h4>
+        </div>
+        <el-input
+            type="textarea"
+            :rows="2"
+            placeholder="请输入内容"
+            v-model="comment_content"
+            maxlength="300"
+            show-word-limit>
+        </el-input>
+        <el-button size="small" style="float:right;margin-top:10px">发表评论</el-button>
+        <div style="clear:both"></div>
         <el-divider></el-divider>
-        <div v-for="comment in comments" :key="comment.id">
+        <div v-for="comment in comments" :key="comment.id" style="text-align:left;">
             <el-avatar shape="square" :size="small" style="float:left"></el-avatar>
             <!-- <div style="clear:both"></div> -->
-            <el-link :underline="false">{{comment.username}}</el-link>
+            <div style="padding-left:50px;position:relative">
+                <el-link :underline="false" type="primary">{{comment.username}}</el-link>
+                <p style="margin-top:6px;font-size:14px;margin-bottom:6px;">{{comment.content}}</p>
+                <!-- <el-row>
+                    <el-col :span="20">
+                        <span style="font-size:14px;line-height:29px;color:#999">{{comment.datetime}}</span>
+                    </el-col>
+                    <el-col :span="4">
+                        <el-button type="text" size="mini">
+                            <i class="zi zi_digg"></i>{{" ("+comment.praise+")"}}
+                        </el-button>
+                    </el-col>
+                </el-row> -->
+                <span style="font-size:14px;line-height:19px;color:#999">{{comment.datetime}}</span>
+                <div style="float:right;line-height:19px;">
+                    <el-link :underline="false" @click="praiseClick" style="margin-right:20px">
+                        <i class="zi zi_digg"></i>{{" ("+comment.praise+")"}}
+                    </el-link>
+                    <el-link :underline="false" @click="praiseClick">
+                        <i class="zi zi_commentalt"></i>
+                    </el-link>
+                </div>
+            </div>
             <el-divider></el-divider>
         </div>
+        <el-pagination
+            background
+            layout="prev, pager, next"
+            :total="1000">
+        </el-pagination>
     </div>
 </template>
 
@@ -29,7 +74,13 @@ export default {
                     datetime:"2012-3-2 14:02:55",
                     praise:10
                 }
-            ]
+            ],
+            comment_content:""
+        }
+    },
+    methods:{
+        praiseClick:function(){
+            console.log("xxx");
         }
     }
 }
