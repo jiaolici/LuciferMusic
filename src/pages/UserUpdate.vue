599d6d58d0a60ca9826cb6a6e49757fa5093a563
jiaolici <1321869892@qq.com>
2020-07-06 23:21:10 +0800

    更新个人信息表单继续完善

--- src/pages/UserUpdate.vue
+++ src/pages/UserUpdate.vue
@@ -1,8 +1,26 @@
 <template>
-    <div>
-        <el-form ref="form" :model="userInfo" label-width="80px" size="small" style="text-align:left;width:460px">
+    <div style="text-align:left;width:460px;margin:100px auto 180px">
+        <h2 style="text-align:center">个人信息</h2>
+        <el-upload
+            action=""
+            :show-file-list="false"
+            style="border-radius:50%;text-align:center;margin-bottom:20px">
+            <div slot="tip" class="el-upload__tip">只能上传jpg/png文件，且不超过500kb</div>
+            <el-avatar :size="100"> user </el-avatar>
+        </el-upload>
+        <el-form ref="form" :model="userInfo" label-width="80px" size="small">
             <el-form-item label="用户名">
                 <el-input v-model="userInfo.name" placeholder="用户名"></el-input>
+            </el-form-item>
+            <el-form-item label="签名">
+                <el-input
+                    type="textarea"
+                    :rows="3"
+                    placeholder="我还没想好要写什么..."
+                    v-model="userInfo.introduction"
+                    maxlength="300"
+                    show-word-limit>
+                </el-input>
             </el-form-item>
             <el-form-item label="性别">
                 <el-radio-group v-model="userInfo.gender">
@@ -19,14 +37,15 @@
                 </el-date-picker>
             </el-form-item>
             <el-form-item label="地区">
-                <el-select v-model="userInfo.area" placeholder="请选择">
-                    <el-option
-                    v-for="item in this.$store.state.globalSetting.areas"
-                        :key="item.id"
-                        :label="item.name"
-                        :value="item.name">
-                    </el-option>
-                </el-select>
+                <el-cascader
+                    v-model="userInfo.area"
+                    :options="this.$store.state.globalSetting.areas"
+                    style="width:100%">
+                </el-cascader>
+            </el-form-item>
+            <el-form-item>
+                <el-button type="primary">保存</el-button>
+                <el-button>取消</el-button>
             </el-form-item>
         </el-form>
     </div>
--- src/store/states.js
+++ src/store/states.js
@@ -8,19 +8,19 @@ const state = {
     globalSetting:{
         areas:[
             {
-                id:1,
-                name:"四川",
-                cities:[
-                    {name:"成都"},
-                    {name:"泸州"},
+                value:"四川省",
+                label:"四川省",
+                children:[
+                    {value:"成都市",label:"成都市"},
+                    {value:"达州市",label:"达州市"},
                 ]
             },
             {
-                id:2,
-                name:"湖北",
-                cities:[
-                    {name:"武汉"},
-                    {name:"襄阳"},
+                value:"湖北省",
+                label:"湖北省",
+                children:[
+                    {value:"武汉市",label:"武汉市"},
+                    {value:"襄阳市",label:"襄阳市"},
                 ]
             },
         ]
