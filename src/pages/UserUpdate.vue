23875e186c4eed0038011323f65b13c7c026dd5d
jiaolici <1321869892@qq.com>
2020-07-06 17:41:30 +0800

    更新个人信息表单完善一下

--- src/pages/UserUpdate.vue
+++ src/pages/UserUpdate.vue
@@ -1,7 +1,7 @@
 <template>
     <div>
-        <el-form ref="form" :model="userInfo" style="padding:20px 60px">
-            <el-form-item>
+        <el-form ref="form" :model="userInfo" label-width="80px" size="small" style="text-align:left;width:460px">
+            <el-form-item label="用户名">
                 <el-input v-model="userInfo.name" placeholder="用户名"></el-input>
             </el-form-item>
             <el-form-item label="性别">
@@ -9,6 +9,24 @@
                     <el-radio label="男"></el-radio>
                     <el-radio label="女"></el-radio>
                 </el-radio-group>
+            </el-form-item>
+            <el-form-item label="生日">
+                <el-date-picker
+                    v-model="userInfo.birthday"
+                    type="date"
+                    placeholder="选择日期"
+                    style="width:100%">
+                </el-date-picker>
+            </el-form-item>
+            <el-form-item label="地区">
+                <el-select v-model="userInfo.area" placeholder="请选择">
+                    <el-option
+                    v-for="item in this.$store.state.globalSetting.areas"
+                        :key="item.id"
+                        :label="item.name"
+                        :value="item.name">
+                    </el-option>
+                </el-select>
             </el-form-item>
         </el-form>
     </div>
@@ -23,7 +41,7 @@ export default {
                 gender:"",
                 birthday:"",
                 introduction:"",
-                city:""
+                area:""
             }
         }
     }
--- src/store/states.js
+++ src/store/states.js
@@ -4,6 +4,26 @@ const state = {
         playingList:[],
         playingSong:null,
         isPlaying:false
+    },
+    globalSetting:{
+        areas:[
+            {
+                id:1,
+                name:"四川",
+                cities:[
+                    {name:"成都"},
+                    {name:"泸州"},
+                ]
+            },
+            {
+                id:2,
+                name:"湖北",
+                cities:[
+                    {name:"武汉"},
+                    {name:"襄阳"},
+                ]
+            },
+        ]
     }
 }
 
